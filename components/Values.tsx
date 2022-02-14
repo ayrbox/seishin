15f31a451ba8202ffc6c1e2529233e7a47e4cb8c
ayrbox <sabin.dangol@hotmail.com>
2022-02-14 21:43:56 +0000

    add rounded corner.

--- components/Values.tsx
+++ components/Values.tsx
@@ -14,13 +14,17 @@ const Values = () => {
           <strong> NAT</strong>, <strong>JLPT </strong>
           exams preparation classes.
         </p>
-
         <div className="flex justify-center items-center p-8 ">
           <div className="w-28 m-3">
             <Image src={JLPTLogo} objectFit="cover" alt="JLPT Logo" />
           </div>
           <div className="w-28 m-3">
-            <Image src={IELTSLogo} alt="IELTS Logo" objectFit="cover" />
+            <Image
+              src={IELTSLogo}
+              alt="IELTS Logo"
+              objectFit="cover"
+              className="rounded-md"
+            />
           </div>
         </div>
       </div>
