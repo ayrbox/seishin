cb7b9074a65f0501840c12b872b8bb47dd0b5c1f
ayrbox <sabin.dangol@hotmail.com>
2022-03-15 21:18:46 +0000

    Hide navbar on mobile.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -28,7 +28,7 @@ const NavBar = () => {
     <nav className="w-screen p-6 bg-transparent transition-all">
       <div className="container mx-auto flex justify-between align-middle">
         <h1 className="text-2xl font-bold text-slate-300">Seishin Education</h1>
-        <ul className="flex justify-between">
+        <ul className="hidden md:flex justify-between">
           {links.map((item) => (
             <NavBarItem key={item.id} linkTo={item.linkTo} label={item.label} />
           ))}
